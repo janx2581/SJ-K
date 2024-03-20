5c0afc7a7c61b52087f33282260ca0810d16d7c3
janx2581 <143491736+janx2581@users.noreply.github.com>
2024-03-21 00:45:49 +0100

    Update rag_chat.py

--- rag_chat.py
+++ rag_chat.py
@@ -28,7 +28,7 @@ import os
 #api_key = input("Please enter your OpenAI API key: ")
 
 # Set the API key as an environment variable
-os.environ["OPENAI_API_KEY"] = api_key
+#os.environ["OPENAI_API_KEY"] = api_key
 
 # Optionally, check that the environment variable was set correctly
 #print("OPENAI_API_KEY has been set!")
