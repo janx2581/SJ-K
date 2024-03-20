5e06001f7fec706f7f45c5435d9ca3b07eb3dac7
janx2581 <143491736+janx2581@users.noreply.github.com>
2024-03-21 00:30:29 +0100

    Update rag_chat.py

--- rag_chat.py
+++ rag_chat.py
@@ -45,8 +45,6 @@ os.environ["OPENAI_API_KEY"] = api_key
 # Optionally, check that the environment variable was set correctly
 #print("OPENAI_API_KEY has been set!")
 
-     
-OPENAI_API_KEY has been set!
 
 llm_model = "gpt-3.5-turbo"
      
