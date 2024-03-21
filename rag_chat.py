449ef0e5af82908556e5b357ccbd2826c58159f0
janx2581 <143491736+janx2581@users.noreply.github.com>
2024-03-21 10:55:48 +0100

    Update rag_chat.py

--- rag_chat.py
+++ rag_chat.py
@@ -48,7 +48,7 @@ from langchain.text_splitter import CharacterTextSplitter
      
 
 #txt_file_path = '/content/drive/MyDrive/scalexi.txt'
-txt_file_path = '/janx2581/SJ-K/SJ-K.txt'
+txt_file_path = 'https://github.com/janx2581/SJ-K/blob/main/SJ-K.txt'
 loader = TextLoader(file_path=txt_file_path, encoding="utf-8")
 data = loader.load()
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
