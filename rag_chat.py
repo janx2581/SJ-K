08295a86dbbbc2ae452b7169191e70376dd4ee21
janx2581 <143491736+janx2581@users.noreply.github.com>
2024-03-21 10:43:50 +0100

    Update rag_chat.py

--- rag_chat.py
+++ rag_chat.py
@@ -12,12 +12,11 @@ from langchain.vectorstores import DocArrayInMemorySearch
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
-#from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain.indexes import VectorstoreIndexCreator
 from langchain_experimental.agents.agent_toolkits.csv.base import create_csv_agent
 from langchain.agents.agent_types import AgentType
-from langchain.memory import ConversationBufferMemorye
 import tiktoken
 from langchain_openai import ChatOpenAI
 #print("done importing")
