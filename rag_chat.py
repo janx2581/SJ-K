4a15384cafa8008ddcdf46ebb27a5fcec127af1b
janx2581 <143491736+janx2581@users.noreply.github.com>
2024-06-12 14:42:24 +0200

    Update rag_chat.py

--- rag_chat.py
+++ rag_chat.py
@@ -1,3 +1,8 @@
+"""
+from google.colab import drive
+drive.mount('/content/drive/')
+"""
+
 import streamlit as st
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
@@ -13,20 +18,45 @@ from langchain_experimental.agents.agent_toolkits.csv.base import create_csv_age
 from langchain.agents.agent_types import AgentType
 import tiktoken
 from langchain_openai import ChatOpenAI
+# print("done importing")
 import openai
-import os
-from langchain.text_splitter import CharacterTextSplitter
 
-# Set up API key
+import os
+
+
+# openai_api_key = st.secrets["OPENAI_API_KEY"]
 api_key = st.secrets["OPENAI_API_KEY"]
 os.environ["api_key"] = st.secrets["OPENAI_API_KEY"]
 
-# Load and split the text file
-txt_file_path = 'SJ-K.txt' #My thesis
+
+# Prompt the user for their OpenAI API key
+# api_key = input("Please enter your OpenAI API key: ")
+
+# Set the API key as an environment variable
+# os.environ["OPENAI_API_KEY"] = api_key
+
+# Optionally, check that the environment variable was set correctly
+# print("OPENAI_API_KEY has been set!")
+
+
+llm_model = "gpt-3.5-turbo"
+
+from langchain.text_splitter import CharacterTextSplitter
+
+# print("done importing the CharacterTextSplitter")
+
+
+# txt_file_path = '/content/drive/MyDrive/scalexi.txt'
+txt_file_path = 'SJ-K.txt'
 loader = TextLoader(file_path=txt_file_path, encoding="utf-8")
 data = loader.load()
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 data = text_splitter.split_documents(data)
+
+# Data
+# print("data")
+# data
+
 
 # Create vector store
 embeddings = OpenAIEmbeddings()
@@ -34,8 +64,9 @@ vectorstore = FAISS.from_documents(data, embedding=embeddings)
 
 # Create conversation chain
 llm = ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo")
-# llm = ChatOpenAI(temperature=0.7, model_name="gpt-4")  # Uncomment if using GPT-4
-memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+# llm = ChatOpenAI(temperature=0.7, model_name="gpt-4")
+memory = ConversationBufferMemory(
+    memory_key='chat_history', return_messages=True)
 conversation_chain = ConversationalRetrievalChain.from_llm(
     llm=llm,
     chain_type="stuff",
@@ -49,7 +80,7 @@ def get_answer(query):
 
 
 # Streamlit app layout
-st.title("John")
+st.title("Thesis Assistant: SJ-K RAG Model")
 st.markdown("""
 Welcome to the Thesis Assistant for SJ-K. This tool is designed to help you quickly understand the key points from the thesis without needing to read the entire document. Simply click on a predefined question or enter your own to get started.
 """)
